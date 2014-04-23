92507b3dcd7671f6e61d9aade1b4e862a6b4ccb3
Weitian LI <liweitianux@gmail.com>
2014-04-24 00:50:44 +0800

    * added blank templates of needed pages * added urls in pages.urls * updated links in navbar.html

--- page/urls.py
+++ page/urls.py
@@ -11,7 +11,36 @@ urlpatterns = patterns('',
     # index page
     url(r'^$', RedirectView.as_view(url=reverse_lazy('index'))),
     # introduction page
-    url(r'^introduction/$', TemplateView.as_view(template_name='page/introduction.html'),
+    url(r'^introduction/$',
+        TemplateView.as_view(template_name='page/introduction.html'),
         name='introduction'),
+    # program page
+    url(r'^program/$',
+        TemplateView.as_view(template_name='page/program.html'),
+        name='program'),
+    # committee page
+    url(r'^committee/$',
+        TemplateView.as_view(template_name='page/committee.html'),
+        name='committee'),
+    # sponsor page
+    url(r'^sponsor/$',
+        TemplateView.as_view(template_name='page/sponsor.html'),
+        name='sponsor'),
+    # traffic page
+    url(r'^traffic/$',
+        TemplateView.as_view(template_name='page/traffic.html'),
+        name='traffic'),
+    # accommodation page
+    url(r'^accommodation/$',
+        TemplateView.as_view(template_name='page/accommodation.html'),
+        name='accommodation'),
+    # contact page
+    url(r'^contact/$',
+        TemplateView.as_view(template_name='page/contact.html'),
+        name='contact'),
+    # about page
+    url(r'^about/$',
+        TemplateView.as_view(template_name='page/about.html'),
+        name='about'),
 )
 
