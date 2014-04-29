a2b75fd0ae62b484c3a7cc1205a1425a91b43533
Weitian LI <liweitianux@gmail.com>
2014-04-29 13:29:38 +0800

    modified 'wsgi.py': set DJANGO_SETTINGS_MODULE to settings_production

--- django_skaschool/wsgi.py
+++ django_skaschool/wsgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_skaschool.settings")
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_skaschool.settings")
+# use settings_production
+os.environ["DJANGO_SETTINGS_MODULE"] = "django_skaschool.settings_production"
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
